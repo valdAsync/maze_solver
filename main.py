362d893f93bc9d8b24c65afbf89210f6086f6656
valdAsync <valdasync@proton.me>
2024-09-11 21:39:49 +0300

    refactor cells

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from graphics import Cell, Line, Point, Window
+from cell import Cell
+from graphics import Line, Point, Window
 
 
 def main():
     win = Window(800, 600)
-    line = Line(Point(50, 50), Point(400, 400))
-    win.draw_line(line, "black")
-    cell = Cell(50, 50, 400, 400, win)
-    cell.draw()
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
 
 
