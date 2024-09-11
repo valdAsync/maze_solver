cd114ae7940160f35757546a22c8297dd4549574
valdAsync <valdasync@proton.me>
2024-09-11 21:32:02 +0300

    add cells

--- graphics.py
+++ graphics.py
@@ -46,3 +46,45 @@ class Line:
         canvas.create_line(
             self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
         )
+
+
+class Cell:
+    def __init__(
+        self,
+        _x1,
+        _y1,
+        _x2,
+        _y2,
+        _win,
+        has_left_wall=True,
+        has_top_wall=True,
+        has_right_wall=True,
+        has_bottom_wall=True,
+    ):
+        self._x1 = _x1
+        self._y1 = _y1
+        self._x2 = _x2
+        self._y2 = _y2
+        self._win = _win
+        self.has_left_wall = has_left_wall
+        self.has_top_wall = has_top_wall
+        self.has_right_wall = has_right_wall
+        self.has_bottom_wall = has_bottom_wall
+
+    def draw(self):
+        if self.has_left_wall:
+            self._win.draw_line(
+                Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            )
+        if self.has_top_wall:
+            self._win.draw_line(
+                Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            )
+        if self.has_right_wall:
+            self._win.draw_line(
+                Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            )
+        if self.has_bottom_wall:
+            self._win.draw_line(
+                Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            )
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from graphics import Line, Point, Window
+from graphics import Cell, Line, Point, Window
 
 
 def main():
     win = Window(800, 600)
     line = Line(Point(50, 50), Point(400, 400))
     win.draw_line(line, "black")
+    cell = Cell(50, 50, 400, 400, win)
+    cell.draw()
     win.wait_for_close()
 
 
