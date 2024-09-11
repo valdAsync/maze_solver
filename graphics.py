9823562318618f914d3730a82bb643023b6a63d3
valdAsync <valdasync@proton.me>
2024-09-11 21:18:23 +0300

    clean up

--- graphics.py
+++ graphics.py
@@ -5,10 +5,10 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -20,25 +20,29 @@ class Window:
             self.redraw()
         print("window closed...")
 
-    def close(self):
-        self.__running = False
-
     def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
 
+    def close(self):
+        self.__running = False
+
 
 class Point:
-    def __init__(self, x=0, y=0):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
 
 class Line:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
+        self.p1 = p1
+        self.p2 = p2
 
     def draw(self, canvas, fill_color="black"):
         canvas.create_line(
-            self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
         )
