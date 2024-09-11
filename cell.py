60bc7a19a67ce88fd86d7ec3ccd3eb992da5cd4a
valdAsync <valdasync@proton.me>
2024-09-11 22:38:10 +0300

    formatting

--- cell.py
+++ cell.py
@@ -35,6 +35,10 @@ class Cell:
             self._win.draw_line(line)
 
     def draw_move(self, to_cell, undo=False):
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
         half_length = abs(self._x2 - self._x1) // 2
         x_center = half_length + self._x1
         y_center = half_length + self._y1
@@ -42,10 +46,6 @@ class Cell:
         half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
         x_center2 = half_length2 + to_cell._x1
         y_center2 = half_length2 + to_cell._y1
-
-        fill_color = "red"
-        if undo:
-            fill_color = "gray"
 
         line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
         self._win.draw_line(line, fill_color)
